fd8e69c5f28b1d1820dd7d6831847d7a8741e200
Enzo Bombardelli <enzobombardellies@gmail.com>
2026-08-24 21:37:13 -0300

    Mais atividades adicionadas

---
+++ Agregacao/Principal.java
@@ -0,0 +1,20 @@
+package Agregacao;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Enzo", "Atacante", 22);
+        Jogador jogador2 = new Jogador("Maria", "Meio-campista", 8);
+
+        Time time = new Time("São Paulo Futebol Clube", "São Paulo", jogador1);
+        System.out.println("DADOS INICIAIS");
+        time.apresentarTime();
+        System.out.println("\nTROCA DE CAPITÃO");
+        time.trocarCapitao(jogador2);
+        System.out.println("\nDADOS ATUALIZADOS");
+        time.apresentarTime();
+        System.out.println("ANTIGO CAPITÃO CONTINUA EXISTINDO");
+        jogador1.apresentarJogador();
+    }
+}
